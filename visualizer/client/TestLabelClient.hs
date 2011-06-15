50a7586c6ae1e27c970471e3e57141fa22b1ff90
Takayuki Muranushi <muranushi@gmail.com>
2011-06-15 17:51:09 +0900

    More interesting visualizer

--- visualizer/client/TestLabelClient.hs
+++ visualizer/client/TestLabelClient.hs
@@ -3,11 +3,17 @@ import System.Posix.Unistd
 
 main :: IO ()
 main = do
-  sub 1
+  sub 0 1
   
-sub :: Integer -> IO ()  
-sub x = do
-  usleep 100000
-  putStrLn $ "label 100 100 1 " ++ show x
-  sub $ (3*x+1) `mod` 2^(40::Int)
+sub :: Double -> Integer -> IO ()  
+sub t n = do
+  let t' = t + 0.01
+      n' = (3*n+1) `mod` 2^(40::Int)
+      draw x y n'' = putStrLn $ unwords ["label", show x, show y, "1", show n'']
+  draw (0.5 * (1 + cos t)) (0.5 * (1 + sin t)) n
+  draw (0.5 * (1 + cos (2*t))) (0.5 * (1 + sin (pi * t))) n
+  draw (0.5 * (1 + cos t)) (0.5 * (1 - sin t)) (3*n)
+  putStrLn "flush"
+  usleep 10000
+  sub t' n'
   
