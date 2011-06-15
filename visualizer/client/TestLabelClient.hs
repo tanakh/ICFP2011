4039b0dcc4ef07ecf602525d1daf2a8bcca65dbc
Takayuki Muranushi <muranushi@gmail.com>
2011-06-15 19:28:02 +0900

    a 60fps client conformal to the visualization server

--- visualizer/client/TestLabelClient.hs
+++ visualizer/client/TestLabelClient.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS -Wall #-}
+import System.IO
 import System.Posix.Unistd
 
 main :: IO ()
@@ -8,12 +9,13 @@ main = do
 sub :: Double -> Integer -> IO ()  
 sub t n = do
   let t' = t + 0.01
-      n' = (3*n+1) `mod` 2^(40::Int)
+      n' = n+1
       draw x y n'' = putStrLn $ unwords ["label", show x, show y, "1", show n'']
   draw (0.5 * (1 + cos t)) (0.5 * (1 + sin t)) n
   draw (0.5 * (1 + cos (2*t))) (0.5 * (1 + sin (pi * t))) n
   draw (0.5 * (1 + cos t)) (0.5 * (1 - sin t)) (3*n)
   putStrLn "flush"
-  usleep 10000
+  hFlush stdout
+  usleep 16000
   sub t' n'
   
