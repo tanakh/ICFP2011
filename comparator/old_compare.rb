2d0c8509a1da406f56b58729bb30b37b1803a9ca
muranushi@gmail.com <nushio@p01.(none)>
2011-06-18 07:38:58 +0900

    backup

---
+++ comparator/old_compare.rb
@@ -0,0 +1,134 @@
+#!/usr/bin/env ruby
+
+$: << '.'
+require 'succinct'
+
+
+LogDir = '/tmp/log/'
+SampleDir = 'sample/'
+GoodFn = 'goodnews.log'
+BadFn  = 'badnews.log'
+
+def sh(cmd)
+  STDERR.puts cmd
+  system(cmd)
+end
+
+def randSlot()
+  return [1,2,4,8,16,32,rand(256)].map{|x| [rand, x]}.sort[0][1]
+end
+
+def randSeed()
+  return rand(2**30)
+end
+
+def diff(fns)
+  hands0 = nil
+  hands1 = nil
+
+  open(fns[0],'r'){|fp| hands0 = succinct_fp(fp) }
+  open(fns[1],'r'){|fp| hands1 = succinct_fp(fp) }
+    
+  p = 0
+  t = 1
+  hands0.length.times{|i|
+    if hands0[i] != hands1[i]
+      unless hands0[i] && hands1[i]
+        return <<MSG
+hand number mismatch in #{p} turn #{t} 
+---
+#{hands0[i].inspect}
+---
+#{hands1[i].inspect}
+---
+
+MSG
+      end
+        
+      return <<MSG
+wrong hand in player #{p} turn #{t} 
+---
+#{hands0[i].join(' ')}
+---
+#{hands1[i].join(' ')}
+---
+MSG
+    end
+    p = 1-p
+    t+=1 if p==0
+  }
+
+  return false
+end
+  
+
+
+
+if ARGV.length < 2
+  STDERR.puts <<USAGE
+./compare.rb siulator1 simulator2 ['./ai1 with args' ['./ai2 with args']]
+
+if ./ai1 or ./ai2 is not specified, ./Random is used.
+USAGE
+exit
+end
+
+system("ln -s ../nushio/Random Random")
+system("ln -s ../nushio/UnstableSayaka UnstableSayaka")
+`mkdir -p #{LogDir}`
+`mkdir -p #{SampleDir}`
+
+sims = ARGV[0..1]
+
+def randomAI()
+  if rand() < 0.5
+    return "./Random #{randSlot} #{randSeed()}" 
+  else
+    rr = 10.0 ** (-5.0*rand())
+    return "./UnstableSayaka #{rr} #{randSlot} #{randSeed()}" 
+  end
+end
+
+def make_ai
+  ret = []
+  ret << (ARGV[2] || randomAI() )
+  ret << (ARGV[3] || randomAI() )
+  return ret
+end
+
+
+loop {
+  ais = make_ai
+  
+  uniquetag = "#{rand(2**30)}"
+  cmds = []
+  fns = []
+  (0..1).each{|simid|
+    fn = "#{LogDir}#{uniquetag}-#{simid}"
+    cmd = "#{sims[simid]} match '#{ais[0]}' '#{ais[1]}' &> #{fn}"
+    sh cmd
+    cmds << cmd
+    fns << fn
+  }
+
+  if reason = diff(fns)
+    msg = "BAD!! #{cmds[0]} != #{cmds[1]}\n#{reason}"
+    STDERR.puts msg
+    open(BadFn, 'a') {|fp|
+      fp.puts Time::now
+      fp.puts msg
+    }
+    sh "cp #{fns[0]} #{fns[1]} #{SampleDir}"
+  else
+    msg = "good. #{cmds[0]} == #{cmds[1]}"
+    STDERR.puts msg
+    open(GoodFn, 'a') {|fp|
+      fp.puts Time::now
+      fp.puts msg
+    }
+  end
+
+  fns.each{|fn|
+    sh "rm #{fn}"
+  }
+}
