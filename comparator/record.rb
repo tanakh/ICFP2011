2e8e4ba0be72692dc01f3ac0ace7f7403041f1dd
muranushi@gmail.com <nushio@p01.(none)>
2011-06-18 07:47:56 +0900

    recording

---
+++ comparator/record.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+
+if ARGV.length < 2
+  STDERR.puts <<USAGE
+./recorder log-filename ./program
+
+excecute the ./program, and records its standard output to log-filename.
+USAGE
+  exit
+end
+
+require 'open3'
+
+LogFn = ARGV[0]
+Prog  = ARGV[1..-1].join(' ')
+
+Open3.popen3(Prog) {|stdin, stdout, stderr|
+  ti = Thread.new do
+    while line = STDIN.gets
+      stdin.puts line
+      stdin.flush
+    end
+    stdin.close
+  end
+
+  to = Thread.new do
+    while line = stdout.gets
+      STDOUT.puts line
+      STDOUT.flush
+    end
+  end
+
+  te = Thread.new do
+    while line = stderr.gets
+      STDERR.puts line
+      STDERR.flush
+    end
+  end
+
+  ti.join
+  to.join
+  te.join
+}
