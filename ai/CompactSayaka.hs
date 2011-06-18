f25b4b0d379b5c6d8b557eb2b397e2972319ffc6
muranushi@gmail.com <nushio@p01.(none)>
2011-06-18 13:35:03 +0900

    Soul Gem is clean

--- ai/CompactSayaka.hs
+++ ai/CompactSayaka.hs
@@ -1,153 +1,9 @@
 {-# OPTIONS -Wall #-}
 import LTG 
+import LTG.SoulGems
 
--- Love me do
--- v[field] <- v[field] v[0]
--- S (K v[field]) Get Zero = v[field] (Get Zero)
--- Construct cost: 4
--- Execution cost: -
-apply0 :: Int -> LTG ()
-apply0 field = do
-  K $> field
-  S $> field
-  field $< Get
-  field $< Zero
 
--- v[field] <- I
-clear :: Int -> LTG()
-clear field = do
-  Zero $> field
 
--- v[field] <- n
-num :: Int -> Int -> LTG ()
-num field n = do
-  clear field
-  field $< Zero
-  num_iter n
-  where
-    num_iter 0 = do
-      return ()
-    num_iter 1 = do
-      Succ $> field
-    num_iter q = do
-      num_iter (q `div` 2)
-      Dbl $> field
-      if q `mod` 2 == 0 
-        then return ()
-        else (Succ $> field) >> (return ())
-
--- execute "attack from to value" using v[0] and v[1]
--- Example: attack 3 4 10
-attack :: Int -> Int -> Int -> LTG ()
-attack from to value = do
-  -- v[1] <- (Attack from)
-  num 1 from
-  Attack $> 1
-  -- v[0] <- to
-  num 0 to
-  -- v[1] <- apply v[1] v[0]
-  apply0 1
-  -- v[0] <- value
-  num 0 value
-  -- v[1] <- apply v[1] v[0]
-  apply0 1
-
--- v[0] <- v[field]
--- Construct cost: 2
--- Execution cost: -
-copytozero :: Int -> LTG ()
-copytozero field = do
-  if field /= 0
-    then do
-      num 0 field
-      Get $> 0
-    else
-      return ()
-
--- v[f1] <- (\x -> v[f1] v[f2])
--- (S (K v[f1]) (K v[f2])) = (\x -> v[f1] v[f2])
--- Construct cost: 3+2+4=9
--- Execution cost: +3
-lazy_apply :: Int -> Int -> LTG ()
-lazy_apply f1 f2 = do
-  K    $> f1
-  S    $> f1
-  copytozero f2
-  K    $> 0
-  apply0 f1
-
--- v[f1] <- \x -> (v[f1] (v[f2] x))
--- S (K v[f1]) v[f2]
-composition :: Int -> Int -> LTG ()
-composition f1 f2 = do
-  K    $> f1
-  S    $> f1
-  copytozero f2
-  apply0  f1
-
--- v[field] <- \x -> v[field] (\x + n)
-lazy_add :: Int -> Int -> LTG ()
-lazy_add field n = do
-  num_iter n
-  where
-    num_iter 0 = do
-      return ()
-    num_iter 1 = do
-      clear 0
-      0     $< Succ
-      composition field 0
-    num_iter q = do
-      if q `mod` 2 == 0 
-        then
-          return ()
-        else do
-           clear 0
-           0     $< Succ
-           composition field 0
-      clear 0
-      0     $< Dbl
-      composition field 0
-      num_iter (q `div` 2)
-
--- v[f1] <- composion v[f1] v[0] ...(n times)... v[0]
-composition0_ntimes :: Int -> Int -> LTG ()
-composition0_ntimes f1 n = do
-  if n <= 0
-    then
-      return ()
-    else do
-      K    $> f1
-      S    $> f1
-      apply0  f1
-      composition0_ntimes f1 (n-1)
-
--- apply0:       v[field] <- v[field] (Get Zero)
--- future_apply: v[f1]    <- \x -> (Get (i1+x)) (Get (i2+x))
--- S (\x -> get (succ ... (succ \x))) (\x -> get (succ ... (succ \x))) zero
--- S (compisition get succ ... succ) (compisition get succ ... succ) zero
-future_apply :: Int -> Int -> Int -> Int -> LTG ()
-future_apply f1 f2 i1 i2 = do
-
-  clear f1
-  f1 $< Get
-  lazy_add f1 i1
-
-  clear f2
-  f2 $< Get
-  lazy_add f2 i2
-
-  S $> f1
-
-  copytozero f2
-  apply0 f1
-
--- v[f1] <- (\x -> get target)
-lazy_get :: Int -> Int -> Int -> LTG ()
-lazy_get target f1 f2 = do -- (S (K Get) (K target)) = (\x -> get target) to f1
-  clear f1
-  f1   $< Get
-  num f2 target
-  lazy_apply f1 f2
 
 -- inject zombie that attack
 inject_sayasaya :: Int -> Int -> Int -> Int -> Int -> LTG ()
@@ -167,7 +23,7 @@ inject_sayasaya f1 f2 fdmg fgain dmg = do
   -- Inject!
   f2 $< Zero
   Zombie $> f2
-  copytozero f1
+  copyTo0 f1
   apply0 f2
 
 -- I want to revive Sayaka!
@@ -188,15 +44,15 @@ revive_sayasaya f1 f2 = do
   Succ   $> 145
   Revive $> 145
 
-  lazy_get f2 f1 f2
+  lazyGet f2 f1 f2
   S    $> f1
   f1   $< Succ
   clear f2
   f2   $< Revive -- the function you wish to execute!
   S    $> f2
-  copytozero f1
+  copyTo0 f1
   apply0 f2
-  copytozero f2
+  copyTo0 f2
   0   $< Zero
   num 0 128
   num f1 f2
--- ai/LTG/SoulGems.hs
+++ ai/LTG/SoulGems.hs
@@ -1,8 +1,19 @@
-module Soulgem (
-                apply0
+module LTG.SoulGems (
+                apply0,
+                clear,
+                num,
+                attack,
+                copyTo0,
+                lazyApply,
+                compose,
+                lazyAdd,
+                composeNtimes0,
+                futureApply,
+                lazyGet
 ) where
-import LTG
 
+import LTG.Base
+import LTG.Monad
 
 -- Love me do
 -- v[field] <- v[field] v[0]
@@ -15,3 +26,142 @@ apply0 field = do
   S $> field
   field $< Get
   field $< Zero
+
+
+-- v[field] <- I
+clear :: Int -> LTG()
+clear field = do
+  Zero $> field
+
+
+-- v[field] <- n
+num :: Int -> Int -> LTG ()
+num field n = do
+  clear field
+  field $< Zero
+  num_iter n
+  where
+    num_iter 0 = do
+      return ()
+    num_iter 1 = do
+      Succ $> field
+    num_iter q = do
+      num_iter (q `div` 2)
+      Dbl $> field
+      if q `mod` 2 == 0 
+        then return ()
+        else (Succ $> field) >> (return ())
+
+
+-- execute "attack from to value" using v[0] and v[1]
+-- Example: attack 3 4 10
+attack :: Int -> Int -> Int -> LTG ()
+attack from to value = do
+  -- v[1] <- (Attack from)
+  num 1 from
+  Attack $> 1
+  -- v[0] <- to
+  num 0 to
+  -- v[1] <- apply v[1] v[0]
+  apply0 1
+  -- v[0] <- value
+  num 0 value
+  -- v[1] <- apply v[1] v[0]
+  apply0 1
+
+-- v[0] <- v[field]
+-- Construct cost: 2
+-- Execution cost: -
+copyTo0 :: Int -> LTG ()
+copyTo0 field = do
+  if field /= 0
+    then do
+      num 0 field
+      Get $> 0
+    else
+      return ()
+
+-- v[f1] <- (\x -> v[f1] v[f2])
+-- (S (K v[f1]) (K v[f2])) = (\x -> v[f1] v[f2])
+-- Construct cost: 3+2+4=9
+-- Execution cost: +3
+lazyApply :: Int -> Int -> LTG ()
+lazyApply f1 f2 = do
+  K    $> f1
+  S    $> f1
+  copyTo0 f2
+  K    $> 0
+  apply0 f1
+
+-- v[f1] <- \x -> (v[f1] (v[f2] x))
+-- S (K v[f1]) v[f2]
+compose :: Int -> Int -> LTG ()
+compose f1 f2 = do
+  K    $> f1
+  S    $> f1
+  copyTo0 f2
+  apply0  f1
+
+-- v[field] <- \x -> v[field] (\x + n)
+lazyAdd :: Int -> Int -> LTG ()
+lazyAdd field n = do
+  num_iter n
+  where
+    num_iter 0 = do
+      return ()
+    num_iter 1 = do
+      clear 0
+      0     $< Succ
+      compose field 0
+    num_iter q = do
+      if q `mod` 2 == 0 
+        then
+          return ()
+        else do
+           clear 0
+           0     $< Succ
+           compose field 0
+      clear 0
+      0     $< Dbl
+      compose field 0
+      num_iter (q `div` 2)
+
+-- v[f1] <- composion v[f1] v[0] ...(n times)... v[0]
+composeNtimes0 :: Int -> Int -> LTG ()
+composeNtimes0 f1 n = do
+  if n <= 0
+    then
+      return ()
+    else do
+      K    $> f1
+      S    $> f1
+      apply0  f1
+      composeNtimes0 f1 (n-1)
+
+-- apply0:       v[field] <- v[field] (Get Zero)
+-- future_apply: v[f1]    <- \x -> (Get (i1+x)) (Get (i2+x))
+-- S (\x -> get (succ ... (succ \x))) (\x -> get (succ ... (succ \x))) zero
+-- S (compisition get succ ... succ) (compisition get succ ... succ) zero
+futureApply :: Int -> Int -> Int -> Int -> LTG ()
+futureApply f1 f2 i1 i2 = do
+
+  clear f1
+  f1 $< Get
+  lazyAdd f1 i1
+
+  clear f2
+  f2 $< Get
+  lazyAdd f2 i2
+
+  S $> f1
+
+  copyTo0 f2
+  apply0 f1
+
+-- v[f1] <- (\x -> get target)
+lazyGet :: Int -> Int -> Int -> LTG ()
+lazyGet target f1 f2 = do -- (S (K Get) (K target)) = (\x -> get target) to f1
+  clear f1
+  f1   $< Get
+  num f2 target
+  lazyApply f1 f2
