f2081a169e6fc78222dc24baaa233e1889d83f18
muranushi@gmail.com <nushio@p01.(none)>
2011-06-18 14:27:23 +0900

    create submisshion

--- ai/create_submission.rb
+++ ai/create_submission.rb
@@ -1,3 +1,17 @@
-git checkout-index -a -f --prefix=export/
+#!/usr/bin/env ruby
+
+def sh(cmd)
+  STDERR.puts cmd
+  system(cmd)
+end
+
+WorkDir = '.work/'
+
+"git checkout-index -a -f --prefix=ai/#{WorkDir}"
+
+
+
+
+
 
 
