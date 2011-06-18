053f2a9d2d7a0099feb8b47c6e72515e48074bfd
muranushi@gmail.com <nushio@p01.(none)>
2011-06-18 14:36:26 +0900

    submission creater completed

--- ai/create_submission.rb
+++ ai/create_submission.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 def sh(cmd)
   STDERR.puts cmd
   system(cmd)
@@ -7,21 +9,31 @@ end
 
 unless ARGV[0]
   STDERR.puts <<USAGE
-#{__FILE__} ./BinaryFileToSubmit [archive_name.tar.gz]
+#{__FILE__} : creates package, ready for official submission, calculate checksum.
+
+usage:
+    #{__FILE__} ./BinaryFileToSubmit [submit.tar.gz]
 USAGE
+  exit
 end
 
 WorkDir   = '.work/'
 BinFn     = ARGV[0]
 ArchiveFn = ARGV[1] || 'submit.tar.gz'
 
+sh "rm -fr #{WorkDir}"
 sh "git checkout-index -a -f --prefix=ai/#{WorkDir}"
 sh "cp install #{WorkDir}"
 sh "cp #{BinFn} #{WorkDir}"
 sh "cp ../README #{WorkDir}"
 
+FileUtils.cd(WorkDir) {
+  sh "tar czhf #{ArchiveFn} *"
+}
 
-
+sh "mv #{WorkDir}/#{ArchiveFn} ."
+sh "rm -fr #{WorkDir}"
+sh "sha512sum #{ArchiveFn}"
 
 
 
