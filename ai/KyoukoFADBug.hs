362ab579687382bad80dcd00d1543e6427938e8a
muranushi@gmail.com <nushio@p01.(none)>
2011-06-19 01:47:49 +0900

    debugging

--- ai/KyoukoFADBug.hs
+++ ai/KyoukoFADBug.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS -Wall #-}
+import Control.Applicative
 import Control.Monad
+import Control.Monad.State(get)
 import LTG 
 
 lazyApplyFAold :: Int -> Int -> Int -> Int -> Int -> LTG ()
 lazyApplyFAold i1 i2 ffa f1 f2 = do
+  lprint $ ("haitta", i1, i2, ffa, f1, f2)
   lazyApplyFA i1 i2 ffa f1 f1 f2
-
+  lprint $ "deta"
 
 findFirstAlive :: Int -> LTG Int
 findFirstAlive i = do
@@ -18,7 +22,7 @@ findFirstAlive i = do
         else findFirstAlive (i+1)
 
 getTotalDamage :: Int -> Int -> Int -> LTG Int
-getTotalDamage d i total = do
+getTotalDamage !d !i !total = do
   if i >= 256
     then return total
     else do
@@ -30,7 +34,7 @@ getTotalDamage d i total = do
           else getTotalDamage d (i+1) (total+d+d)
 
 getOptimumDamage :: Int -> Int -> Int -> Int -> LTG (Int, Int)
-getOptimumDamage i d bestD bestTotal = do
+getOptimumDamage !i !d !bestD !bestTotal = do
   t <- getTotalDamage d i 0
   if d >= 10000 || t == 0
     then return (t, bestD)
@@ -50,17 +54,25 @@ zombieLoopFA f2 f3 f6 f7 = do
         (total, dmg) <- getOptimumDamage i 1 1 0
         return (total, dmg, i)
 
+  lprint "Zhiru0"
   alives <- filterM isAlive' [0..255]
+  lprint "Zhiru1"
   cands  <- mapM eval' alives
+  lprint "Zhiru2"
   let (_, dmg, firstI) = maximum cands
-
+  lprint "Zhiru3"
   num f6 dmg
+  lprint "Zhiru4"
   num f7 firstI
+
+  tc <- turnCnt <$> get
+  lprint $ ("turn",tc)
 
   copyTo f3 f2
   f3 $< I
 
 
+  lprint "Zhiru()"
   zombieLoopFA f2 f3 f6 f7
 
 
@@ -75,6 +87,7 @@ kyokoAnAnFA i1 i2 ffa f1 f2 f3 f4 f6 f7 target = do
   clear f2
   f2 $< Copy
   lazyApplyFAold i1 i2 ffa f2 f1
+  lprint $ "point1"
   S $> f2
   f2 $< Succ
 
@@ -93,6 +106,7 @@ kyokoAnAnFA i1 i2 ffa f1 f2 f3 f4 f6 f7 target = do
   f3 $< Copy
   num f1 f6
   lazyApplyFAold i1 i2 ffa f3 f1
+  lprint $ "point2"
   applyFA i1 i2 ffa f4 f4 f3
 
  -- v[f4] <- S f next
@@ -105,12 +119,14 @@ kyokoAnAnFA i1 i2 ffa f1 f2 f3 f4 f6 f7 target = do
   f1 $< Copy
   num f2 f4
   lazyApplyFAold i1 i2 ffa f1 f2
+  lprint $ "point3"
   S $> f1
 
   clear f2
   f2 $< Copy
   num f3 f7
   lazyApplyFAold i1 i2 ffa f2 f3
+  lprint $ "point4"
   applyFA i1 i2 ffa f1 f1 f2
 --  lazyApply2 f1 f2 f3
 
@@ -118,6 +134,7 @@ kyokoAnAnFA i1 i2 ffa f1 f2 f3 f4 f6 f7 target = do
   Zombie $> f2
 
   lazyApplyFAold i1 i2 ffa f2 f1
+  lprint $ "point5"
   zombieLoopFA f2 f3 f6 f7
 
 
@@ -133,10 +150,8 @@ main = runLTG $ do
   attack      1 0 8192
   attack      2 0 8192
 
--- v[5] <- S (S help I) (lazyApply Copy 6)
-
+              
+  lprint "anan surua"
   kyokoAnAnFA 1 2 4 3 5 9 8 6 7 255
+  lprint "anan owata"
 
---  attackFA    1 2 18 3 5 6 8192
---  attackLoopFA 1 2 18 5 0 0
---  sittingDuck
