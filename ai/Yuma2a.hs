3964727f097d1d5b206b57c282b4de868300a544
Hiroshige Hayashizaki <hayashizaki.kitsune@gmail.com>
2011-06-20 00:49:18 +0900

    Yuma2a updated

--- ai/Yuma2a.hs
+++ ai/Yuma2a.hs
@@ -15,65 +15,58 @@ sittingDuck = do
 
 kyoukoMain :: LTG()
 kyoukoMain = do
+  let f1 = 1
+  let f3 = 2
   -- S (K (S (S Attack (K 255)) (K 8192))) Get
-  clear 1
-  1 $< S
-  1 $< Attack
+
+  -- v[f1] <- S Attack (K 255)
+  clear f1
+  f1 $< S
+  f1 $< Attack
   num 0 255
-  copyTo 2 0
   K $> 0
-  apply0 1
-  S $> 1
+  apply0 f1
   0 $< Put
+
+  -- v[f1] <- S (S Attack (K 255)) (K 255*2)
+  S   $> f1
+  Dbl $> 0
+  K   $> 0
+  apply0 f1
+  0 $< Put
+
+  -- v[f3] <- S (S Help I) (K 255*32)
+  Dbl $> 0
+  Dbl $> 0
+  Dbl $> 0
   Dbl $> 0
   K $> 0
-  apply0 1
+  clear f3
+  f3 $< S
+  f3 $< Help
+  f3 $< I
+  S  $> f3
+  apply0 f3
 
-  0 $< Put
-  Dbl $> 0
-  Dbl $> 0
-  Dbl $> 0
-  Dbl $> 0
-  K $> 0  
-  clear 3
-  3 $< S
-  3 $< Help
-  3 $< I
-  S $> 3
-  apply0 3
-
+  -- v[f3] <- (S v[f3] (S Get I))
   clear 0
   0 $< S
   0 $< Get
   0 $< I
-  S $> 3
-  apply0 3
+  S $> f3
+  apply0 f3
 
-  S $> 1
-  copyTo 0 3
-  apply0 1
+  -- S v[f1] v[f3]
+  S $> f1
+  copyTo 0 f3
+  apply0 f1
 
-  copyTo 0 1
+  copyTo 0 f1
 
-  num 2 1
-  Get $> 2
-  2 $< Zero
-
-  num 2 1
-  Get $> 2
-  2 $< Zero
-
-  num 2 1
-  Get $> 2
-  2 $< Zero
-
-  num 2 1
-  Get $> 2
-  2 $< Zero
-
---  num 0 2
---  1 $< Zero
-
+  forever $ do
+    num 2 f1
+    Get $> 2
+    2 $< Zero
 
   sittingDuck
 
