d3e93837324c2fd1e77b7a27792a6bd8502748d5
muranushi@gmail.com <nushio@p01.(none)>
2011-06-19 12:20:28 +0900

    sinogeru

--- ai/Revive255.hs
+++ ai/Revive255.hs
@@ -1,18 +1,43 @@
 #!/usr/bin/env runhaskell
-import LTG
 
 import Control.Monad
+import LTG
+import Data.IORef
+import System.IO.Unsafe
+
+
+hima, busy :: IORef Int
+hima = unsafePerformIO $ newIORef 0
+busy = unsafePerformIO $ newIORef 0
+{-# NOINLINE hima #-}
+{-# NOINLINE busy #-}
 
 main :: IO ()
-main = runLTG ltgMain
+main = runLTG $ do
+         num 1 255
+         num 0 1
+         Get $> 0
+         forever recover
 
-ltgMain :: LTG ()
-ltgMain = do
-  num 1 255
-  Get $> 0
-  forever $ do
-    b <- isAlive True 255
-    if b then nop
-    else do
-      Revive $> 0
-      Get $> 0
+recover :: LTG ()
+recover = do
+  a0 <- isAlive True 0
+  a1 <- isAlive True 1
+  a255 <- isAlive True 255
+  [f0, f1] <- mapM (getField True) [0,1]
+  if (a0 && a1 && f0 == VInt 255 && f1 == VInt 255) 
+  then do 
+    if a255 then nop
+    else Revive $> 0
+  else do 
+    when (not a0) $ revive 0 >> return ()
+    when (not a1) $ revive 1 >> return ()
+    when (f0 /= VInt 255 && f1 == VInt 255) $ do 
+            num 0 1
+            Get $> 0
+    when (f0 == VInt 255 && f1 /= VInt 255) $ do 
+            num 1 0
+            Get $> 1
+  
+
+  
