5bcefe26f9e0aa9d6c1772cbfd3a3b526655baca
muranushi@gmail.com <nushio@p01.(none)>
2011-06-20 19:41:40 +0900

    ready for manual operation of YAUJ!

--- league/polling.rb
+++ league/polling.rb
@@ -1,18 +1,24 @@
 #!/usr/bin/env ruby
 
 require 'open-uri'
+require 'fileutils'
 
 RegisterURL = 'http://www.paraiso-lang.org/Walpurgisnacht/register.txt'
 ResultFn = 'result.txt'
 ParticipantsFn = 'participants_yaoj.txt'
 ParticipantsFn2 = 'participants.txt'
+Ptgz = 'p.tgz'
 
 YaujDir = 'yauj/'
 
-def sh(string)
-  STDERR.puts string
+ExecMode = ARGV.index('-X')
+
+def sh(str)
+  STDERR.puts str
   system(str)
 end
+
+
 
 class Participant
   def initialize(n,url)
@@ -21,13 +27,20 @@ class Participant
   end
   attr_accessor :name, :url
 
-  def runpath()
-    YaujDir + @name + '/run'
+  def localdir()
+    YaujDir + @name + '/'
   end
+  def packagepath()
+    localdir() + Ptgz
+  end
+  def runpath()
+    localdir() + 'run'
+  end
+
 end
 
 
-regs = []
+yaojParticipants = []
 
 open(RegisterURL, 'r') { |fp|
   counter = 0
@@ -37,14 +50,14 @@ open(RegisterURL, 'r') { |fp|
     str = words[0..-4].join(' ')
     name = counter.to_s + "_" + str.gsub(/\W/,'')
     url = words[-1]
-    regs << Participant::new(name, url)
+    yaojParticipants << Participant::new(name, url)
     counter +=1
   end
 }
 
 open(ParticipantsFn,'w') {|fp|
-  regs.each {|participant|
-    fp.puts participant.runpath 
+  yaojParticipants.each {|pants|
+    fp.puts pants.runpath 
   }
   open(ParticipantsFn2, 'r') {|fp2|
     while line = fp2.gets
@@ -52,3 +65,30 @@ open(ParticipantsFn,'w') {|fp|
     end
   }
 }
+
+yaojParticipants.each{|pants|
+  sh "rm -fr #{pants.localdir}"
+  sh "mkdir #{pants.localdir}"
+  sh "wget #{pants.url} -q  -O #{pants.packagepath}"
+  FileUtils.cd(pants.localdir) {
+    sh "tar zxf #{Ptgz}"
+    contents = `tar tf #{Ptgz}`.split(/\n/)
+    flag = false
+    contents.each{|fn|
+      words = fn.split('/')
+      if words[-1] == 'install'
+        flag = true# found
+        if words.length >= 2
+          path = words[0..-2].join('/')
+          sh "mv #{path}/* ."
+        end
+        break
+      end
+    } # end search for
+    unless flag
+      STDERR.puts "!! cannot recognize package for #{pants.name}"
+      next
+    end
+    sh "install" if ExecMode
+  } # exit participant directory
+} # participant loop
