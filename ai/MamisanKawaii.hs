1778253c65295efc9b491822dc059460fe65e672
nushio <muranushi@gmail.com>
2011-06-25 16:38:55 +0900

    Mamisan no miryoku wo amasutokoronaku hyougen shitemimasita

--- ai/MamisanKawaii.hs
+++ ai/MamisanKawaii.hs
@@ -8,8 +8,13 @@ import Data.IORef
 import System.IO.Unsafe
 
 
-i1, i2, i3, i255 :: Int
-(i1: i2: i3: _) = [1..]
+attackN, attackDmg, helpN :: Int
+attackN   = 11112
+attackDmg = 10000
+helpN     =  9999
+
+i1, i2, i3, i4, i255 :: Int
+(i1: i2: i3: i4: _) = [1..]
 i255 = 255
 
 
@@ -22,9 +27,10 @@ isOpening = liftIO $ readIORef openingSwitch
 
 main :: IO ()
 main = runLTGs
-       [(isOpening      , openingMove),
-        (isAlive False 0, forever $ i1 $<< Get) ,
-        (return True    , hima)
+       [(isOpening                   , openingMove),
+        (isAlive False 0             , discipline) ,
+        ((<55000) <$> getVital True 0, pumpUp),
+        (return True                 , shoot)
        ]
 
 
@@ -58,12 +64,15 @@ numCost :: Int -> Int
 numCost 0 = 1
 numCost i = 1 + (i `mod` 2) + (numCost $ i `div` 2)
 
-hima :: LTG ()
-hima = forever $ do
-         t <- getTurnCnt
-         b <- getVital False 0
-         lprint $ "hima t: " ++ show t ++ " " ++ show b
-         nop
+-- get 3 * 2^n or 2^n smaller than x
+getEasyInt :: Int -> Int
+getEasyInt x | (x <= 3) = x
+getEasyInt x = 
+  max (head $ filter (\y -> y * 2 > x) twos) (head $ filter (\y -> y * 2 > x) threep)
+  where
+    twos = map (2^) [(0::Int)..]
+    threep = 1 : map (\n -> 3*(2^n)) [(0::Int)..]
+
 
 {- Ikki ni Kimesasete
    Morauwayo ! -}
@@ -123,3 +132,85 @@ openingMove = do
   liftIO $ writeIORef openingSwitch False
 
   forever nop
+
+
+{- Otonashiku shite ireba
+   Kaeri ni chanto kaihou shiteageru. -}
+discipline :: LTG ()
+discipline = forever $ i1 $<< Get
+
+{- Oshikattawane! Tiro - - - -}
+pumpUp :: LTG ()
+pumpUp =
+  forever $ do
+    (v, _, i) <- maximum <$> mapM f [1..255]
+    if v > helpN
+    then do
+      ensureAlive i4
+      num i4 i
+      Help $>> i4
+      i4 $<< Zero
+      ensureAlive i3
+      num i3 helpN
+      copyTo 0 i3
+
+      debug
+      applyR0 i4
+    else do
+      ensureAlive i4
+      num i4 0
+      Help $>> i4
+      i4 $<< Zero
+      v0 <- getVital True 0
+      num 0 (getEasyInt v0)
+
+      debug
+      applyR0 i4
+        where        
+          f i = do
+              v <- getVital True i
+              return (v, -numCost i, i)
+
+{- - - - Finale! -}
+shoot :: LTG ()
+shoot =
+  forever $ do
+    (v, _, i) <- maximum <$> mapM f [1..255]
+    if v >= attackDmg
+    then do
+      ensureAlive i4
+      num i4 0
+      Attack $>> i4
+      num 0 (255-i)
+      applyR0 i4
+      ensureAlive i2
+      num i2 attackN
+      copyTo 0 i2
+
+      debug
+      applyR0 i4
+    else do
+      ensureAlive i4
+      i4 $<< Zero
+      Attack $>> i4
+      num 0 (255-i)
+      applyR0 i4
+      ve <- getVital False i
+      num 0 (getEasyInt (2*ve))
+
+      f4 <- getField True i4 
+      f0 <- getField True 0
+      lprint $ show f4 ++ " $ "  ++ show f0
+      
+      debug
+      applyR0 i4
+        where        
+          f i = do
+              v <- getVital False i
+              return (v, numCost i, i)
+
+
+debug = do
+  f4 <- getField True i4 
+  f0 <- getField True 0
+  lprint $ show f4 ++ " $ "  ++ show f0
