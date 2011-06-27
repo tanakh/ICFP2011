5de31434a1385afea25b4cf00480570f69c1654a
nushio <muranushi@gmail.com>
2011-06-27 12:02:44 +0900

    improved killer

--- league/mass-kill.sh
+++ league/mass-kill.sh
@@ -1,9 +1,11 @@
 /home/management/p.nodes  --xp ssh {n} killall ghc
 /home/management/p.nodes  --xp ssh {n} killall ltg
 /home/management/p.nodes --xp ssh {n} killall ruby
+/home/management/p.nodes --xp ssh {n} killall run
 
 sleep 10
 
 /home/management/p.nodes --xp ssh {n} killall -9 ghc
 /home/management/p.nodes --xp ssh {n} killall -9 ltg
 /home/management/p.nodes --xp ssh {n} killall -9 ruby
+/home/management/p.nodes --xp ssh {n} killall -9 run
